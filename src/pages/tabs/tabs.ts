4771ee5c74bb249d33b23d155f3b67a6c217c651
Abishai Parise <abishai.parise@gmail.com>
2017-10-11 23:32:02 -0500

    Add camera in Tab Page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -50,136 +50,50 @@ export class TabsPage {
   }
 
   openCamera(){
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      quality : 95,
+      saveToPhotoAlbum: true
+    }).then((imageData) => {
+        console.log(imageData);
+        let imageurl = 'data:image/jpeg;base64,'+imageData;
 
-      let actionSheet = this.actionSheetCtrl.create({
-        title: 'Select Image Source',
-        buttons: [
-          {
-            text: 'Take Picture',
-            handler: () => {
-              this.camera.getPicture({
-                sourceType: this.camera.PictureSourceType.CAMERA,
-                destinationType: this.camera.DestinationType.DATA_URL,
-                quality : 95,
-                saveToPhotoAlbum: true
-              }).then((imageData) => {
-                  console.log(imageData);
-                  let imageurl = 'data:image/jpeg;base64,'+imageData;
+        let storageRef = firebase.storage().ref('/images');
 
-                  let storageRef = firebase.storage().ref('/videos');
-
-                  const imageRef = storageRef.child(firebase.auth().currentUser.uid);
-                  imageRef.putString(imageurl, firebase.storage.StringFormat.DATA_URL).then((res: any)=> {
+        const imageRef = storageRef.child(firebase.auth().currentUser.uid);
+        imageRef.putString(imageurl, firebase.storage.StringFormat.DATA_URL).then((res: any)=> {
 
 
 
-                    storageRef.child(firebase.auth().currentUser.uid).getDownloadURL().then((url) => {
+          storageRef.child(firebase.auth().currentUser.uid).getDownloadURL().then((url) => {
 
 
 
 
-                      let alert = this.alertCtrl.create({
-                        title: 'Url!',
-                        subTitle: url,
-                        buttons: ['OK']
-                      });
-                      alert.present();
-                      // this.chatservice.addnewmessage(url).then(() => {
-                      //   this.scrollto();
-                      //   this.newmessage = '';
-                      // })
-                    }).catch((err) => {
-                    });
-                  }).catch((err) => {
+            let alert = this.alertCtrl.create({
+              title: 'Url!',
+              subTitle: url,
+              buttons: ['OK']
+            });
+            alert.present();
+            // this.chatservice.addnewmessage(url).then(() => {
+            //   this.scrollto();
+            //   this.newmessage = '';
+            // })
+          }).catch((err) => {
+          });
+        }).catch((err) => {
 
-                  })
-
-
-
-                },
-                (err: CaptureError) => console.error(err)
-              );
+        })
 
 
 
-
-            }
-          },
-          {
-            text: 'Take Video',
-            handler: () => {
-
-              let options: CaptureVideoOptions = { limit: 3 };
-              this.mediaCapture.captureVideo(options)
-                .then((data: MediaFile[]) => {
-                    console.log(data);
-                    for(let entry of data){
-                      let alert = this.alertCtrl.create({
-                        title: 'Updated!',
-                        subTitle: JSON.stringify(entry.fullPath),
-                        buttons: ['OK']
-                      });
-                      alert.present();
-                      let imageurl = "file://"+entry.fullPath;
-                      let storageRef = firebase.storage().ref('/videos');
+      },
+      (err: CaptureError) => console.error(err)
+    );
 
 
-                      (<any>window).FilePath.resolveNativePath(imageurl, (result) => {
-                        this.nativepath = result;
-                        (<any>window).resolveLocalFileSystemURL(this.nativepath, (res) => {
-                          res.file((resFile) => {
-                            var reader = new FileReader();
-                            reader.readAsArrayBuffer(resFile);
-                            reader.onloadend = (evt: any) => {
-                              var imgBlob = new Blob([evt.target.result], { type: 'video/mov' });
-
-                              const imageRef = storageRef.child(firebase.auth().currentUser.uid);
-                              // imageRef.putString(imageurl, firebase.storage.StringFormat.DATA_URL).then((res: any)=> {
-                              imageRef.put(imgBlob).then((res: any)=> {
-
-                                storageRef.child(firebase.auth().currentUser.uid).getDownloadURL().then((url) => {
-                                  let alert = this.alertCtrl.create({
-                                    title: 'Url!',
-                                    subTitle: url,
-                                    buttons: ['OK']
-                                  });
-                                  alert.present();
-                                  // this.chatservice.addnewmessage(url).then(() => {
-                                  //   this.scrollto();
-                                  //   this.newmessage = '';
-                                  // })
-                                }).catch((err) => {
-                                });
-                              }).catch((err) => {
-
-                              })
-                                .catch((err) => {
-
-                                })
-                            }
-                          })
-                        })
-                      })
-
-                    }
-
-
-                  },
-                  (err: CaptureError) => console.error(err)
-                );
-
-
-
-
-            }
-          },
-          {
-            text: 'Cancel',
-            role: 'cancel'
-          }
-        ]
-      });
-      actionSheet.present();
   }
 
 }
